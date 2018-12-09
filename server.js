8e1013ed9750eef512e1d3fb043d637bc5071815
Kevin Cho <chokevinj@gmail.com>
2018-12-09 13:36:56 -0500

    Add production setup

--- server.js
+++ server.js
@@ -11,8 +11,18 @@ const db = low(adapter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// API Endpoints
 app.get('/api/content', (req, res) => {
   res.send({ data: db.get('content') });
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
